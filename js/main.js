04900413a69005e949349902fd7bf628da4fb4c2
Chally <carlosgilino@gmail.com>
2024-10-03 11:32:31 +0200

    add api fetch function. add filters btn event listeners

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let ideasBtn = document.querySelector('.ideas-btn');
+let filtersBtn = document.querySelectorAll('nav a');
+const APIURL = 'https://bored-api.appbrewery.com/random';
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 ideasBtn.addEventListener('mousedown', () => {
     if (ideasBtn) {
@@ -11,4 +14,38 @@ ideasBtn.addEventListener('mouseup', () => {
         ideasBtn.classList.remove('ideas-btn--active');
 
     }
+})
+
+
+ideasBtn.addEventListener('click', () => {
+    if (ideasBtn) {
+        fetch(PROXY_URL + APIURL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error in response. Please try again');
+                }
+                return response.json();
+
+            })
+
+            .then(data => {
+                console.log(data.key);
+            })
+
+            .catch(error => {
+                console.error('No fue posible obtener los datos ', error);
+            });
+    }
+
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    filtersBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            console.log(btn.textContent);
+
+        });
+    });
+
 })
