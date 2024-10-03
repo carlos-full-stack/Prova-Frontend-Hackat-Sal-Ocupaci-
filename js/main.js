6f0c7943e907ae1871643da3948791ad472aca56
Chally <carlosgilino@gmail.com>
2024-10-03 13:51:14 +0200

    add js modifications

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 let ideasBtn = document.querySelector('.ideas-btn');
 let filtersBtn = document.querySelectorAll('nav a');
-const APIURL = 'https://bored-api.appbrewery.com/random';
+const APIURL = 'https://bored-api.appbrewery.com/';
 const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+
+
 
 ideasBtn.addEventListener('mousedown', () => {
     if (ideasBtn) {
@@ -18,34 +20,64 @@ ideasBtn.addEventListener('mouseup', () => {
 
 
 ideasBtn.addEventListener('click', () => {
-    if (ideasBtn) {
-        fetch(PROXY_URL + APIURL)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error in response. Please try again');
-                }
-                return response.json();
-
-            })
-
-            .then(data => {
-                console.log(data.key);
-            })
-
-            .catch(error => {
-                console.error('No fue posible obtener los datos ', error);
-            });
-    }
-
+    if (ideasBtn) callApi();
 })
 
 
 document.addEventListener('DOMContentLoaded', () => {
     filtersBtn.forEach(btn => {
         btn.addEventListener('click', () => {
-            console.log(btn.textContent);
+
+            callApi(btn.textContent.toLowerCase());
+
 
         });
     });
 
 })
+
+
+function callApi(filter) {
+
+    let url;
+
+    if (filter) {
+        url = PROXY_URL + APIURL + `filter?type=${filter}`;
+    } else {
+        url = PROXY_URL + APIURL + 'random';
+    }
+
+    fetch(url)
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error in response. Please try again');
+            }
+            return response.json();
+
+        })
+
+        .then(data => {
+
+            if (filter) {
+                updateActivity(data[0].activity);
+            } else {
+                updateActivity(data.activity);
+            }
+
+        })
+
+        .catch(error => {
+            console.error('No fue posible obtener los datos ', error);
+        });
+
+    console.log(filter);
+}
+
+
+function updateActivity(name) {
+
+    let activityName = document.querySelector('.col-2 p');
+    if (activityName) activityName.innerHTML = name;
+
+}
